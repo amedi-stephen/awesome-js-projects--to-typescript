3c74fa580da979b8c56083ddfa490635b9b336d8
steveamedi <amsteve46@gmail.com>
2021-05-25 17:38:24 +0300

    working on configuration and counter app

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+const displayCount = document.getElementById("count");
+const decreaseBtn = document.getElementById("decrease");
+const increaseBtn = document.getElementById("increase");
+const resetBtn = document.getElementById("reset");
+let count = 0;
+increaseBtn?.addEventListener("click", increaseCount);
+function increaseCount() {
+}
+//# sourceMappingURL=index.js.map
